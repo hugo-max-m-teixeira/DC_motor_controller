d4571e67aac3d92785cdfdeb32e160546ecb8fe4
hugo-max-m-teixeira <hugomaxmontelesteixeira@gmail.com>
2022-07-20 20:25:42 -0300

    Deleted some unnecessary lines

--- DC_motor_controller.cpp
+++ DC_motor_controller.cpp
@@ -8,17 +8,6 @@
 
 #include <DC_motor_controller.h>
 
-// cria uma outra classe (ponteiro)
-DC_motor_controller *pointer_motor;
-
-static void motor_interrupt (){
-	pointer_motor->isr();
-}
-/*
-void DC_motor_controller::setInterrupt(){
-	//attachInterrupt(digitalPinToInterrupt(encoderPinA), interrupt, FALLING);
-}*/
-
 void DC_motor_controller::hBridge(uint8_t in1, uint8_t in2, uint8_t en){
   this-> in1 = in1;
   this-> in2 = in2;
@@ -31,7 +20,6 @@ void DC_motor_controller::setPins(){
   pinMode(en, OUTPUT);
   pinMode(encoderPinA, INPUT);
   pinMode(encoderPinB, INPUT);
-  attachInterrupt(digitalPinToInterrupt(encoderPinA), motor_interrupt, FALLING);
 }
 
 void DC_motor_controller::run(int pwm){
